e182bdede2a540487a1850c262e4d41a188ed1dd
mohamed66786 <mohamedtarek7080@gmail.com>
2023-12-29 03:09:06 +0200

    add new features

--- backend/app.ts
+++ backend/app.ts
@@ -1,8 +1,28 @@
-import express from "express";
-export const app= express();
+require("dotenv").config();
+import express, { NextFunction, Request, Response } from "express";
+export const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
 // body Parser
-app.use(express.json({limit:"50mb"}));
+app.use(express.json({ limit: "50mb" }));
 // cookie parser
+app.use(cookieParser());
+// using cors =>cross origin resource sharing
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+// test api
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({ success: true, message: "api test successful" });
+});
+
+// for unknown routes
+app.get("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found}`) as any;
+  err.statusCode = 404;
+  next(err);
+});
