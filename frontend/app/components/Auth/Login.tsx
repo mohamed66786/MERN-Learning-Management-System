e32e80690320ecba903f7be372a564fa3421848d
mohamed667866 <mohamedtarek70900@gmail.com>
2024-12-10 15:16:18 +0200

    creating login component

--- frontend/app/components/Auth/Login.tsx
+++ frontend/app/components/Auth/Login.tsx
@@ -1,11 +1,49 @@
-import React from 'react'
+"use clint";
+import React, { FC, useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import {
+  AiOutlineEye,
+  AiOutlineEyeInvisible,
+  AiFillGithub,
+} from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { styles } from "@/app/styles/style";
 
-type Props = {}
+type Props = {
+  setRoute: (route: string) => void;
+};
 
-const Login = (props: Props) => {
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email("invalid email!")
+    .required("Please enter your email!"),
+  password: Yup.string().required("Please enter your password!").min(6),
+});
+
+const Login: FC<Props> = (props: Props) => {
+  const [show, setShow] = useState(false);
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: schema,
+    onSubmit: async (email, password) => {
+      console.log(email, password);
+    },
+  });
+
+  const { errors, touched, values, handleChange, handleSubmit } = formik;
+
   return (
-    <div>Login</div>
-  )
-}
+    <div className="w-full">
+      <h1 className={`${styles.title}`}>
+        
+      </h1>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
